93aa02ec6f3fa836ac12e8c1996fe5645468a146
lucena <john@caixabaixa.com>
2014-03-04 16:53:21 -0800

    Added form validation using http://jqueryvalidation.org/.

--- mapreduce-python/static/js/custom.js
+++ mapreduce-python/static/js/custom.js
@@ -14,6 +14,46 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
+$(function() {
+    $.validator.addMethod(
+        "end_greater_than_start",
+        function(value, element) {
+            return $('#sequenceEnd').val() > $('#sequenceStart').val()
+        },
+        "End must be greater than or equal to start.");
+    $.validator.addMethod(
+        "start_less_than_end",
+        function(value, element) {
+            return $('#sequenceStart').val() < $('#sequenceEnd').val()
+        },
+        "Start must be less than or equal to end.");
+    $("#coverageForm").validate({
+        rules: {
+            readsetId: "required",
+            sequenceStart: {
+                required: true,
+                number: true,
+                start_less_than_end: true
+            },
+            sequenceEnd: {
+                required: true,
+                number: true,
+                end_greater_than_start: true
+            }
+        },
+        messages: {
+            readsetId: "Please provide a Readset Id.",
+            sequenceStart: {
+                required: "Please provide a Sequence Start."
+            },
+            sequenceStart: {
+                required: "Please provide a Sequence End."
+            }
+         }
+
+    });
+});
+
 function submitReadProcessing() {
   //$('#submitRead').prop('value', 'Processing...');
   //$('#submitRead').attr("disabled", true);
