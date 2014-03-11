33b9295643f52c9d2781f08f16d3e1f6eec8588c
lucena <john@caixabaixa.com>
2014-03-11 09:47:25 -0700

    Improve validation of sequence start and end.

--- mapreduce-python/static/js/custom.js
+++ mapreduce-python/static/js/custom.js
@@ -18,21 +18,28 @@ $(function() {
     $.validator.addMethod(
         "end_greater_than_start",
         function(value, element) {
-            return parseInt($('#sequenceEnd').val(), 10) > parseInt($('#sequenceStart').val())
+            return parseInt($('#sequenceEnd').val()) >= parseInt($('#sequenceStart').val())
         },
         "End must be greater than or equal to start.");
     $.validator.addMethod(
         "start_less_than_end",
         function(value, element) {
-            return parseInt($('#sequenceStart').val()) < parseInt($('#sequenceEnd').val())
+            return parseInt($('#sequenceStart').val()) <= parseInt($('#sequenceEnd').val())
         },
         "Start must be less than or equal to end.");
+    $.validator.addMethod(
+        "start_greater_than_zero",
+        function(value, element) {
+            return parseInt($('#sequenceStart').val()) > 0
+        },
+        "Start must be greater than 0.");
     $("#coverageForm").validate({
         rules: {
             readsetId: "required",
             sequenceStart: {
                 required: true,
                 number: true,
+                start_greater_than_zero: true,
                 start_less_than_end: true
             },
             sequenceEnd: {
